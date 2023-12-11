70d531c691364af439f8d9659fe912ff3596f81a
vvv9912 <vvv9912@github.com>
2023-12-11 18:15:19 +0300

    iter3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,22 +4,14 @@ import (
 	"context"
 	"github.com/vvv9912/ya-go-musthave-metrics-tpl.git/internal/Server/server"
 	"github.com/vvv9912/ya-go-musthave-metrics-tpl.git/internal/Server/storage"
-	"log"
-	"os"
 )
 
 func main() {
-	f, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Println(err)
-	}
-	logger := log.New(f, "server: ", log.LstdFlags)
-	logger.Println("Server start")
 
 	counter := storage.NewCounterStorage()
 	gauge := storage.NewGaugeStorage()
 
-	s := server.NewServer(logger)
+	s := server.NewServer()
 	ctx := context.TODO()
 	s.StartServer(ctx, "localhost:8080", gauge, counter)
 	done := make(chan struct{})
