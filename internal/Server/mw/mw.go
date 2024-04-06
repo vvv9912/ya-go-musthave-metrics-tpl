579871075ffbdb0d3d87c8445d826aac7e7b5c7c
vvv9912 <you@example.com>
2024-04-06 15:15:17 +0300

    iter16

--- internal/Server/mw/mw.go
+++ internal/Server/mw/mw.go
@@ -89,17 +89,22 @@ func (m *Mw) MwLogger(next http.Handler) http.Handler {
 }
 
 // проверяем, что клиент умеет получать от сервера сжатые данные в определенном формате
+//
+//	func supportAcceptType(acceptType map[string]struct{}, acceptTypeReq string) bool {
+//		if acceptTypeReq == "*/*" {
+//			return true
+//		} else {
+//			for key := range acceptType {
+//				if strings.Contains(acceptTypeReq, key) {
+//					return true
+//				}
+//			}
+//		}
+//		return false
+//	}
 func supportAcceptType(acceptType map[string]struct{}, acceptTypeReq string) bool {
-	if acceptTypeReq == "*/*" {
-		return true
-	} else {
-		for key := range acceptType {
-			if strings.Contains(acceptTypeReq, key) {
-				return true
-			}
-		}
-	}
-	return false
+	_, ok := acceptType[acceptTypeReq]
+	return ok // возвращает true, если ключ существует в мапе, иначе false
 }
 
 // проверяем, что клиент поддерживает соответствующий content-type
@@ -121,6 +126,7 @@ func (m *Mw) MiddlewareGzip(next http.Handler) http.Handler {
 			"application/json": struct{}{},
 			"text/html":        struct{}{},
 			"html/text":        struct{}{}, //
+			"*/*":              struct{}{},
 		}
 
 		supportGzip := map[string]struct{}{
