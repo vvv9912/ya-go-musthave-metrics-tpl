dd132485ad195a0baa59d188e77ed98ddd91742d
VETaskanov <VETaskanov@get-lab.ru>
2024-05-10 12:10:05 +0300

    iter21 fix test

--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestStartNotifyCron(t *testing.T) {
 	metrics := metrics.NewMetriсs()
-	postreq := server.NewPostRequest("")
+	postreq := server.NewPostRequest("", nil)
 	n := notifier.NewNotifier(metrics, postreq, time.Duration(2*time.Second), time.Duration(10*time.Second), "http://localhost:8080")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
